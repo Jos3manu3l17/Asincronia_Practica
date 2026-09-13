8902e861c1e4cd77b64b66aa261fd2ba80c58fd4
Jose Manuel Carreño <josepuerta1345@gmail.com>
2026-09-13 14:47:48 -0500

    Feat: Implementar ejercicio de la primera peticion totalmente funcional, siendo este exportado para ser llamado el archivo barril e importarlo en menu.js, tambien se implemento la estructura del switch en el menu, para seleccionar la opcion deseada

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Exportación de todos los módulos para ser utilizados en otros archivos
+
 export * from "./src/modules/menu.js";
-export * from "./src/modules/Peticion1.js";
-export * from "./src/modules/Peticion2.js";
-export * from "./src/modules/Peticion3.js";
-export * from "./src/modules/Peticion4.js";
-export * from "./src/modules/Peticion5.js";
+export * from "./src/modules/peticion1.js";
+export * from "./src/modules/peticion2.js";
+export * from "./src/modules/peticion3.js";
+export * from "./src/modules/peticion4.js";
+export * from "./src/modules/peticion5.js";
